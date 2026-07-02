ec13e7276131121262834836b8966be67268187b
Aaryan Anand <144722275+AaryanAnand10@users.noreply.github.com>
2026-07-02 10:12:19 +0530

    remove validate

--- temp_validate.py
+++
@@ -1,7 +0,0 @@
-import json
-f = open('heston.ipynb', 'r')
-nb = json.load(f)
-f.close()
-code = sum(1 for c in nb['cells'] if c['cell_type'] == 'code')
-md = sum(1 for c in nb['cells'] if c['cell_type'] == 'markdown')
-print(f"Valid notebook: {len(nb['cells'])} cells total ({code} code, {md} markdown)")
